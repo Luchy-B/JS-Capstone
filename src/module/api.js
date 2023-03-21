46ad52dbd66a0c3a4f5f9a83fe4f05d5dbccf4fa
Luchy-B <okparabon@gmail.com>
2023-03-21 15:55:36 +0100

    Add Api

---
+++ src/module/api.js
@@ -0,0 +1,7 @@
+import COMMENTS from './comments.js';
+import SHOWS from './home.js';
+
+const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
+const appsEndPoint = '/apps/';
+const likesEndPoint = '/likes/';
+const id = 'n2zpESiz5HCZWW1SRiUA';
